0ba6bb14d719c4d52623568e63a1b2a6bb585921
Zalán Tóth <zalant.tsc@gmail.com>
2024-11-20 22:10:37 +0000

    Typo fix

--- Create/createCollections.mongodb.js
+++ Create/createCollections.mongodb.js
@@ -1,6 +1,6 @@
 use ("rifstar");
 
-db.createCollection("currencies")
-db.createCollection("nations")
-db.createCollection("associations")
-db.createCollection("universes")
+db.createCollection("currencies");
+db.createCollection("nations");
+db.createCollection("associations");
+db.createCollection("universes");
