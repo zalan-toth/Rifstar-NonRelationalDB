88add72d24e11bc9e361b250112c83e1a5e3752c
Zalán Tóth <zalant.tsc@gmail.com>
2024-11-20 23:58:15 +0000

    Added more information extraction to aggregate

--- Aggregation/CX/Operations/Avg/avg.mongodb.js
+++ Aggregation/CX/Operations/Avg/avg.mongodb.js
@@ -5,9 +5,21 @@ db.celestials.aggregate([
     {
         $group: {
           _id: "$type",
-          avgDiameter: {
-            $avg: "$diameter"
-          }
+          maxDiameter: {$max: "$diameter"},
+          avgDiameter: {$avg: "$diameter"},
+          minDiameter: {$min: "$diameter"},
+          maxOrbitDistance: {$max: "$orbitDistance"},
+          avgOrbitDistance: {$avg: "$orbitDistance"},
+          minOrbitDistance: {$min: "$orbitDistance"},
+          maxSols: {$max: "$sol"},
+          avgSols: {$avg: "$sol"},
+          minSols: {$min: "$sol"},
+          maxHoursPerSol: {$max: "$hoursPerSol"},
+          avgHoursPerSol: {$avg: "$hoursPerSol"},
+          minHoursPerSol: {$min: "$hoursPerSol"},
+          maxMass: {$max: "$mass"},
+          avgMass: {$avg: "$mass"},
+          minMass: {$min: "$mass"}
         }
     }
 ])
