6f29e5acdf7f7d74354c7cccebd99f8b0503280e
Zalán Tóth <zalant.tsc@gmail.com>
2024-11-20 14:57:18 +0000

    Improved min aggregate added!

--- Aggregation/MaxMin/improvedMin.mongodb.js
+++ Aggregation/MaxMin/improvedMin.mongodb.js
@@ -13,20 +13,31 @@ db.universes.aggregate([
         $unwind: "$galaxies.solarSystems.centralCelestial.orbitingCelestials"
     },
     { 
+        $match: {
+            "galaxies.solarSystems.centralCelestial.orbitingCelestials.diameter": { $ne: null }
+        }
+    },
+    { 
         $project: {
             planet: "$galaxies.solarSystems.centralCelestial.orbitingCelestials._id",
+            planetName: "$galaxies.solarSystems.centralCelestial.orbitingCelestials.name",
             diameter: "$galaxies.solarSystems.centralCelestial.orbitingCelestials.diameter"
         }
     },
     {
-        $group: {
-            _id: null,
-            minDiameter: { $min: "$diameter" }
+        $sort: {
+            diameter: 1 //ascending order
         }
-    }, //we loose data about the corresponding planet this way. :-(
+    },
+    { 
+        $limit: 1
+    }, //we don't loose data about the corresponding planet this way. :-)
     { 
         $project: {
-            _id: 0
+            _id: 0,
+            planet: 1,
+            planetName: 1,
+            diameter: 1
         }
     }
 ])
