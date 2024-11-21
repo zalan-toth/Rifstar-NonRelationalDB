aa8c16e4e9e7054e54851eb6884b19680cf66041
Zalán Tóth <zalant.tsc@gmail.com>
2024-11-21 23:25:47 +0000

    Added experimental exchange rate calculator

--- Aggregation/CX/Operations/Avg/arithmetic.mongodb.js
+++ Aggregation/CX/Operations/Avg/arithmetic.mongodb.js
@@ -1,1 +1,28 @@
-//TODO calculate currency exchange rates
+use ("rifstar");
+
+db.currencies.aggregate([
+    {
+        $lookup: {
+            from: "currencies",
+            pipeline: [],
+            as: "exchange"
+        }
+    },
+    {
+        $unwind: "$exchange"
+    },
+    {
+      $match: {
+        $expr: { $ne: ["$_id", "$exchange._id"] } //same currency exception
+      }
+    },
+    {
+        $project: {
+            from: "$code",
+            to: "$exchange.code",
+            exchangeRate: {
+                $divide: ["$exchange.value", "$value"]
+            }
+        }
+    }
+])
