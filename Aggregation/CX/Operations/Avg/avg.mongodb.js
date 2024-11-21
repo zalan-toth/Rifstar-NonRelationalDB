09c1a77e780e39c38ce81131338ed032dbbfb2f5
Zalán Tóth <zalant.tsc@gmail.com>
2024-11-21 00:05:03 +0000

    Added rounding to an aggregate pipeline

--- Aggregation/CX/Operations/Avg/avg.mongodb.js
+++ Aggregation/CX/Operations/Avg/avg.mongodb.js
@@ -1,6 +1,6 @@
 use ("rifstar");
 
-
+//get average/max/min diameter/orbitdistance/sols/hourspersol/mass for each natural celestial category (star/planet/moon)
 db.celestials.aggregate([
     {
         $group: {
@@ -20,6 +20,25 @@ db.celestials.aggregate([
           maxMass: {$max: "$mass"},
           avgMass: {$avg: "$mass"},
           minMass: {$min: "$mass"}
+        }
+    },
+    { //rounding results to 6 digits
+        $project: {
+            maxDiameter: { $round: ["$maxDiameter", 6] },
+            avgDiameter: { $round: ["$avgDiameter", 6] },
+            minDiameter: { $round: ["$minDiameter", 6] },
+            maxOrbitDistance: { $round: ["$maxOrbitDistance", 6] },
+            avgOrbitDistance: { $round: ["$avgOrbitDistance", 6] },
+            minOrbitDistance: { $round: ["$minOrbitDistance", 6] },
+            maxSols: { $round: ["$maxSols", 6] },
+            avgSols: { $round: ["$avgSols", 6] },
+            minSols: { $round: ["$minSols", 6] },
+            maxHoursPerSol: { $round: ["$maxHoursPerSol", 6] },
+            avgHoursPerSol: { $round: ["$avgHoursPerSol", 6] },
+            minHoursPerSol: { $round: ["$minHoursPerSol", 6] },
+            maxMass: { $round: ["$maxMass", 6] },
+            avgMass: { $round: ["$avgMass", 6] },
+            minMass: { $round: ["$minMass", 6] }
         }
     }
 ])
